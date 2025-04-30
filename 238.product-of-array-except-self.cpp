d606f457c15086dd7a191dbcb007cecccf957172
DevAgnihotri <agnihotrivibhasanjeevdev108@gmail.com>
2025-04-30 22:51:37 +0530

    Updated

--- 238.product-of-array-except-self.cpp
+++ 238.product-of-array-except-self.cpp
@@ -79,21 +79,40 @@ public:
 
         return ans;
     }
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        int suffix = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+
+        for (int j = n - 2; j >= 0; j--)
+        {
+            suffix *= nums[j + 1];
+            ans[j] *= suffix;
+        }
+
+        return ans;
+    }
 };
 
 // Main function should be outside the class
-int main()
-{
-    Solution solution;
-    vector<int> nums = {1, 2, 3, 4};
-    vector<int> result = solution.productExceptSelf(nums);
+// int main()
+// {
+//     Solution solution;
+//     vector<int> nums = {1, 2, 3, 4};
+//     vector<int> result = solution.productExceptSelf(nums);
 
-    for (int num : result)
-    {
-        cout << num << " ";
-    }
-    cout << endl;
+//     for (int num : result)
+//     {
+//         cout << num << " ";
+//     }
+//     cout << endl;
 
-    return 0;
-}
+//     return 0;
+// }
 // @lc code=end
